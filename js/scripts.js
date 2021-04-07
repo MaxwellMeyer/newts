fcb30932b476a709b624888ef42104fd07aff633
Maxwell Meyer <maxreadswell@gmail.com>
2021-04-06 21:22:57 -0700

    add methods to post to board, and give notes unique ids

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,12 @@ function CorkBoard() {
   this.notes = {};
   this.currentId = 0;
 }
+CorkBoard.prototype.postNote = function(note) {
+  note.id = this.assignId();
+  this.notes[note.id] = note;
+}
+CorkBoard.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
 
