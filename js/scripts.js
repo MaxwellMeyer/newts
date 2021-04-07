4cc22b9cd27fb9ef7b98e803b5b0ac21d3e7b8e6
Maxwell Meyer <maxreadswell@gmail.com>
2021-04-06 21:29:56 -0700

    add methods to find and delete a note via id

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,16 @@ CorkBoard.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
 }
-
+CorkBoard.prototype.locateNote = function(id) {
+  if (this.notes[id] != undefined) {
+    return this.notes[id];
+  }
+  return false;
+}
+CorkBoard.prototype.burnNote = function(id) {
+  if (this.notes[id] === undefined) {
+    return false;
+  }
+  delete this.notes[id];
+  return true;
+}
